a2a82ab8f21ac17b40f259e19deb1cc6dcf22546
ilyve <ilyveksle@gmail.com>
2022-01-15 12:55:57 -0800

    Added initial Spring app

---
+++ backend/src/test/java/com/educatr/Educatr/EducatrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educatr.Educatr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducatrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
