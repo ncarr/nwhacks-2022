f02b599ec439bac748c42a0b8dd2e294d1a361e4
Nicholas Carr <ncarr@users.noreply.github.com>
2022-01-16 02:39:11 -0500

    Revert "loginwork"
    This reverts commit 7dcc702c9fb78c0fd0268de7631c2c2111ac8eb2.

--- frontend/pages/api/messages.js
+++ frontend/pages/api/messages.js
@@ -1,18 +1,12 @@
 import {getAccessToken, handleLogin, withApiAuthRequired} from '@auth0/nextjs-auth0';
-import {log} from "util";
 
 export default withApiAuthRequired(async function messages(req, res) {
     // If your Access Token is expired and you have a Refresh Token
     // `getAccessToken` will fetch you a new one using the `refresh_token` grant
 
-    let options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: '{"client_id":"5Zc0qLubPf08TBBPq8mv6eaosYgif99M","client_secret":"1ZR5ldAWsEmDANRPtX3W6GjBHn2mgkNz_RkUQeY27YWN2ZOyzXdHwz7TLSlYg3CH","audience":"https://educatr/api","grant_type":"client_credentials"}' };
-
-    const { accessToken } = await fetch('https://dev-zhlpfa8k.us.auth0.com/oauth/token', options);
-
-    console.log(accessToken)
+    const { accessToken } = await getAccessToken(req, res, {
+        scopes: ['read:messages']
+    });
     const response = await fetch('http://localhost:3010/api/private', {
         headers: {
             Authorization: `Bearer ${accessToken}`
