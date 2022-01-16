93cd5a8343fe36276a466410f79d8d33565486a6
Nicholas Carr <ncarr@users.noreply.github.com>
2022-01-16 11:07:09 -0500

    Added lesson endpoint

--- backend/src/main/java/com/educatr/Educatr/web/APIController.java
+++ backend/src/main/java/com/educatr/Educatr/web/APIController.java
@@ -1,6 +1,13 @@
 package com.educatr.Educatr.web;
 
+import java.util.List;
+
+import com.educatr.Educatr.CustomerRepository;
+import com.educatr.Educatr.LessonFile;
+import com.educatr.Educatr.LessonFileRepository;
 import com.educatr.Educatr.model.Message;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Handles requests to "/api" endpoints.
- * @see com.auth0.example.security.SecurityConfig to see how these endpoints are protected.
+ * 
+ * @see com.auth0.example.security.SecurityConfig to see how these endpoints are
+ *      protected.
  */
 @RestController
 @RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
-// For simplicity of this sample, allow all origins. Real applications should configure CORS for their use case.
+// For simplicity of this sample, allow all origins. Real applications should
+// configure CORS for their use case.
 @CrossOrigin(origins = "*")
 public class APIController {
+
+    @Autowired
+    private LessonFileRepository repository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @GetMapping(value = "/public")
     public Message publicEndpoint() {
@@ -29,6 +44,12 @@ public class APIController {
 
     @GetMapping(value = "/private-scoped")
     public Message privateScopedEndpoint() {
-        return new Message("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
+        return new Message(
+                "All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
+    }
+
+    @GetMapping(value = "/lessons")
+    public List<LessonFile> findAllLessons() {
+        return repository.findAll();
     }
 }
